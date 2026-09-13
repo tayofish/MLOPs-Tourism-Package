bdd97b4365e0cd4c038f74327f63387784e4471e
tayofish <103644199+tayofish@users.noreply.github.com>
2026-09-13 14:24:34 +0100

    Enable wide layout for Streamlit page configuration

--- deployment/hf_space/streamlit.py
+++ deployment/hf_space/streamlit.py
@@ -13,7 +13,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Tourism Wellness Package Prediction",
     page_icon="🌍"
-    # ,layout="wide"
+    ,layout="wide"
 )
 
 # ---------------------------------------------------------
