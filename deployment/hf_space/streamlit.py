51c9dd11ba0cf9bb61d575610b7675e3e81543e8
USERNAME <103644199+tayofish@users.noreply.github.com>
2026-09-13 14:17:44 +0100

    Update streamlit app and artefacts

--- deployment/hf_space/streamlit.py
+++ deployment/hf_space/streamlit.py
@@ -12,8 +12,8 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Tourism Wellness Package Prediction",
-    page_icon="🌍",
-    layout="wide"
+    page_icon="🌍"
+    # ,layout="wide"
 )
 
 # ---------------------------------------------------------
